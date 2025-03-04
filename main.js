32f1a48295988aca3befb582adf981c853c7ea9f
Patrick <162671878+PatrickFuchs1234@users.noreply.github.com>
2025-03-04 15:47:15 +0200

    lastest projects slider

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+$('.slider-container').slick({
+  autoplay: true,        // Do we want it to autoplay? true or false
+	autoplaySpeed: 2000,   // How long between each slide when autoplaying
+	speed: 500,            // How fast is the transition 
+  arrows: true,          // Do you want to show arrows to trigger each slide
+  accessibility: true,   // Enables tabbing and arrow key navigation 
+  dots: true,            // Enables the dots below to show how many slides
+  fade: false,           // Changes the animate from slide to fade if true
+  infinite: true,       // When true, means that it will scroll in a circle
+  pauseOnHover: true,   // When true means the autoplay pauses when hovering
+  pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
 // Skript um Höhen von containern aufeinander abzustimmen
 
 function syncHeight() {

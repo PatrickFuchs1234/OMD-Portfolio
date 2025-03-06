15efd5b5f31e5835c196b9287ea97f2aa56dd731
jennywohlfahrtstaetter <162671255+jennywohlfahrtstaetter@users.noreply.github.com>
2025-03-06 17:23:00 +0200

    js

--- main.js
+++ main.js
@@ -24,26 +24,38 @@ function updateContentToGerman() {
   document.getElementById('mobile').innerText = "Responsive Design";
   document.getElementById('seo').innerText = "SEO-optimiert";
   document.getElementById('fast').innerText = "Schnell";
-  document.getElementById('fast-txt').innerText = "Unser Engagement für schnelle Lösungen resultiert aus der Verwendung modernster Technologien, die sicherstellen, dass Ihre Website mit blitzschneller Geschwindigkeit lädt.";
+  document.getElementById('fast-txt').innerText = "Moderne Techniken sorgen dafür, dass Ihre Website in kürzester Zeit lädt und eine hervorragende Nutzererfahrung bietet.";
   document.getElementById('strategy').innerText = "Strategisch";
-  document.getElementById('strategy-txt').innerText = "Unser Team bringt strategische Erkenntnisse ein, versteht Ihre Geschäftsziele und entwickelt Lösungen, die perfekt darauf abgestimmt sind.";
+  document.getElementById('strategy-txt').innerText = "Wir entwickeln Lösungen, die exakt auf Ihre Geschäftsziele zugeschnitten sind, um langfristigen Erfolg zu sichern. ";
   document.getElementById('responsive').innerText = "Anpassungsfähig";
-  document.getElementById('responsive-txt').innerText = "Wir stellen sicher, dass sich Ihre Website auf allen Geräten durch Responsive-Design-Techniken nahtlos anpasst und die Benutzererfahrung auf allen Plattformen und Bildschirmgrößen optimiert wird.";
+  document.getElementById('responsive-txt').innerText = "Wir stellen sicher, dass sich Ihre Website auf allen Geräten durch Responsive-Design-Techniken nahtlos anpasst und die Benutzererfahrung auf allen Plattformen und Bildschirmgrößen optimiert.";
   document.getElementById('maintenance').innerText = "Eigenwartung";
-  document.getElementById('maintenance-txt').innerText = "Mit unseren benutzerfreundlichen CMS-Lösungen machen wir es Ihnen leicht, Ihre Website unabhängig zu verwalten – ohne technische Kenntnisse.";
-  document.getElementById('unlock').innerText = "Entfesseln Sie Ihr digitales Potenzial";
-  document.getElementById('consultation').innerText = "Starten Sie mit einer kostenlosen Beratung";
-  document.getElementById('about').innerText = "Über das Team";
-  document.getElementById('jenny1').innerText = "Jennys Weg in die Webentwicklung wurde durch ihre Liebe zur Problemlösung und ihrem Wunsch, Kreativität mit technischen Fähigkeiten zu verbinden, inspiriert. Mit ihrem Hintergrund in der Sprachtherapie kombiniert sie analytisches Denken mit einem scharfen Blick für Design, um intuitive Benutzererlebnisse zu gestalten. Abseits des Programmierens liebt Jenny die Fotografie und nutzt ihre Expertise in sozialen Medien, um ansprechende digitale Erlebnisse zu schaffen.";
-  document.getElementById('jenny2').innerText = "Jennys Leidenschaft für Problemlösung und Kreativität führte sie zur Webentwicklung. Mit einem Hintergrund in Sprachtherapie verbindet sie analytisches Denken mit Design, um intuitive Benutzererlebnisse zu schaffen.";
-  document.getElementById('patrick1').innerText = "Mit einem Hintergrund in Bauingenieurwesen und einer Leidenschaft für Technologie bringt Patrick eine einzigartige Perspektive in die Webentwicklung ein. Inspiriert von seinen Reisen und seiner Faszination für digitale Innovationen, hat er den Schritt in die Webentwicklung gewagt. Heute spezialisiert er sich darauf, benutzerzentrierte, intuitive Websites zu entwerfen und zu entwickeln, die nicht nur die Erwartungen der Kunden erfüllen, sondern übertreffen.";
+  document.getElementById('maintenance-txt').innerText = "Mit unseren benutzerfreundlichen CMS-Lösungen machen wir es Ihnen leicht, Ihre Website unabhängig zu verwalten – ganz ohne Programmier-Kenntnisse.";
+  document.getElementById('fast1').innerText = "Schnell";
+  document.getElementById('fast-txt1').innerText = "Moderne Techniken sorgen dafür, dass Ihre Website in kürzester Zeit lädt und eine hervorragende Nutzererfahrung bietet.";
+  document.getElementById('strategy1').innerText = "Strategisch";
+  document.getElementById('strategy-txt1').innerText = "Wir entwickeln Lösungen, die exakt auf Ihre Geschäftsziele zugeschnitten sind, um langfristigen Erfolg zu sichern. ";
+  document.getElementById('responsive1').innerText = "Anpassungsfähig";
+  document.getElementById('responsive-txt1').innerText = "Wir stellen sicher, dass sich Ihre Website auf allen Geräten durch Responsive-Design-Techniken nahtlos anpasst und die Benutzererfahrung auf allen Plattformen und Bildschirmgrößen optimiert.";
+  document.getElementById('maintenance1').innerText = "Eigenwartung";
+  document.getElementById('maintenance-txt1').innerText = "Mit unseren benutzerfreundlichen CMS-Lösungen machen wir es Ihnen leicht, Ihre Website unabhängig zu verwalten – ganz ohne Programmier-Kenntnisse.";
+  document.getElementById('unlock').innerText = "Nutzen Sie das volle Potenzial Ihrer digitalen Möglichkeiten";
+  document.getElementById('consultation').innerText = "Vereinbaren Sie jetzt eine kostenlose Beratung";
+  document.getElementById('about').innerText = "Das Team";
+  document.getElementById('jenny1').innerText = "Jenny ist in die Web-Entwicklung eingestiegen, um technisch komplexe Herausforderungen mit kreativen Lösungen anzugehen. Durch ihr analytisches Denken und ihre Erfahrung in der Sprachtherapie entwicklt sie funktionale, ästhetisch ansprechende und nutzerzentrierte Websites. Abseits des Programmierens bietet Jenny auch Fotografie und Social-Media-Services an, um ansprechende digitale Inhalte zu erstellen.";
+  document.getElementById('jenny2').innerText = "Jenny ist in die Webentwicklung eingestiegen, um kreative Lösungen für komplexe Herausforderungen zu finden. Ihr analytisches Denken und ihre Erfahrung aus der Sprachtherapie helfen ihr, nutzerzentrierte Websites zu entwickeln, die funktional und ästhetisch ansprechend sind.";
+  document.getElementById('patrick1').innerText = "Mit einem Hintergrund in Bauingenieurwesen und einer Leidenschaft für Technologie bringt Patrick eine einzigartige Perspektive in die Webentwicklung ein. Inspiriert von seinen Reisen und seiner Faszination für digitale Innovationen, hat er den Schritt in die Webentwicklung gewagt. Heute konzentriert er sich auf die Gestaltung und Entwicklung benutzerfreundlicher Websites, die die Anforderungen seiner Kunden zuverlässig erfüllen.";
   document.getElementById('patrick2').innerText = "Mit einem Hintergrund im Bauingenieurwesen und einer Leidenschaft für Technologie verbindet Patrick technisches Fachwissen mit einer Leidenschaft für digitale Innovation, um benutzerzentrierte, intuitive Websites zu entwickeln.";
   document.getElementById('projects').innerText = "Neueste Projekte";
   document.getElementById('optimized').innerText = "Optimiert für jedes Gerät";
-  document.getElementById('responsive').innerText = "Responsive Design";
-  document.getElementById('contact1').innerText = "Schreiben Sie uns eine Nachricht ...";
-  document.getElementById('contact2').innerText = "Haben Sie ein Projekt im Kopf oder eine Frage? Wir würden gerne von Ihnen hören! Füllen Sie das Formular unten aus und wir melden uns bald bei Ihnen. Lassen Sie uns gemeinsam etwas Großartiges schaffen!";
+  document.getElementById('responsive1').innerText = "Responsive Design";
+  document.getElementById('contact1').innerText = "Schreib uns eine Nachricht";
+  document.getElementById('contact2').innerText = "Möchten Sie ein Projekt besprechen oder haben Sie eine Frage? Wir freuen uns auf Ihre Nachricht! Füllen Sie einfach das Formular aus, und wir melden uns zeitnah bei Ihnen. Lassen Sie uns zusammen an Ihrer Idee arbeiten!";
   document.getElementById('submit').innerText = "ABSENDEN";
+  document.getElementById('privacy').innerText = "Datenschutz";
+  document.getElementById('legal-notice').innerText = "Impressum";
+  document.getElementById('cookie-notice').innerText = "Cookie-Richtlinie";
+  document.getElementById('location').innerText = "Adresse";
 }
 
 // Function to update content in English
@@ -64,12 +76,20 @@ function updateContentToEnglish() {
   document.getElementById('responsive-txt').innerText = "We ensure your website adapts smoothly to all devices through responsive design techniques, optimizing user experience across platforms and screen sizes.";
   document.getElementById('maintenance').innerText = "Self-Maintenance";
   document.getElementById('maintenance-txt').innerText = "With our user-friendly CMS solutions, we make it easy for you to manage your website independently - no technical expertise required.";
+  document.getElementById('fast1').innerText = "Lightning Fast";
+  document.getElementById('fast-txt1').innerText = "Our commitment to rapid solutions stems from our use of cutting-edge technologies, ensuring your website loads with lightning speed.";
+  document.getElementById('strategy1').innerText = "Strategic Perspective";
+  document.getElementById('strategy-txt1').innerText = "Our team brings strategic insights to the table, understanding your business goals and crafting solutions that align perfectly.";
+  document.getElementById('responsive1').innerText = "Responsive Design";
+  document.getElementById('responsive-txt1').innerText = "We ensure your website adapts smoothly to all devices through responsive design techniques, optimizing user experience across platforms and screen sizes.";
+  document.getElementById('maintenance1').innerText = "Self-Maintenance";
+  document.getElementById('maintenance-txt1').innerText = "With our user-friendly CMS solutions, we make it easy for you to manage your website independently - no technical expertise required.";
   document.getElementById('unlock').innerText = "Unlock your digital potential";
   document.getElementById('consultation').innerText = "get started with a free consultation";
   document.getElementById('about').innerText = "About the team";
   document.getElementById('jenny1').innerText = "Jenny's journey into web development was sparked by her love for problem-solving and her desire to blend creativity with technical skills. Drawing from her speech therapy background, she combines an analytical mindset with a keen eye for design to craft intuitive user experiences. Outside of coding, Jenny enjoys photography and leverages her expertise in social media to create engaging digital experiences.";
   document.getElementById('jenny2').innerText = "Jenny's passion for problem-solving and creativity led her to web development. With a background in speech therapy, she blends analytical thinking with design to create intuitive user experiences.";
-  document.getElementById('patrick1').innerText = "With a foundation in civil engineering and a passion for technology, Patrick brings a unique perspective to web development. Inspired by his travels and a fascination with digital innovation, he transitioned into web development. Today, he specializes in designing and developing intuitive, user-centric websites that not only meet but exceed client expectations.";
+  document.getElementById('patrick1').innerText = "With a foundation in civil engineering and a passion for technology, Patrick brings a unique perspective to web development. Inspired by his travels and a fascination with digital innovation, he transitioned into web development. Today, he specializes in designing and developing intuitive, user-centric websites that meet client expectations.";
   document.getElementById('patrick2').innerText = "With a civil engineering background and a passion for technology, Patrick combines technical expertise with a passion for digital innovation to craft intuitive, user-focused websites.";
   document.getElementById('projects').innerText = "Latest Projects";
   document.getElementById('optimized').innerText = "Optimized for every device";
@@ -77,6 +97,10 @@ function updateContentToEnglish() {
   document.getElementById('contact1').innerText = "Send us a Message";
   document.getElementById('contact2').innerText = "Have a project in mind or a question? We'd love to hear from you! Fill out the form below, and we'll get back to you soon. Let's create something great together!";
   document.getElementById('submit').innerText = "SUBMIT";
+  document.getElementById('privacy').innerText = "Privacy";
+  document.getElementById('legal-notice').innerText = "Legal Notice";
+  document.getElementById('cookie-notice').innerText = "Cookie Notice";
+  document.getElementById('location').innerText = "location";
 }
 
 
